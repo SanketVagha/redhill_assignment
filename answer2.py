f3a1d9371e04f96f80823a051fe6d5665ed1a378
SanketVagha <sanketvaghani9@gmail.com>
2024-01-12 16:11:06 +0530

    Completed Second Question

--- answer2.py
+++ answer2.py
@@ -12,7 +12,7 @@ At least 1 character from [ _ or @ or $ ].
 
 ''' 
 
-
+#  Strong Password Not Found till loop executed
 
 while True:
     isValid = 0
@@ -21,33 +21,33 @@ while True:
     lowerCase = 0
     digit = 0
     special = 0
-    if len(password) < 8:
+    if len(password) < 8:  # check the length of the password
         print("Minimum 8 characters.")
     else:
-        for i in password:
-            if i.isupper():
+        for i in password: # string divided into single charactor
+            if i.isupper():  # upper latter check
                 upperCase += 1
-            elif i.islower():
+            elif i.islower(): # lower latter check
                 lowerCase += 1
-            elif i.isdigit():
+            elif i.isdigit():  # digit check 
                 digit += 1
-            elif i == '_' or i == '@' or i == '$':
+            elif i == '_' or i == '@' or i == '$':  # given special charactor _ @ $ check 
                 special += 1
-            else:
+            else:                   # any other charactor find out in password break the loop and send the error message
                 print("Invalid Character")
                 isValid = 1
                 break
         if isValid == 0:
-            if upperCase > 0 and lowerCase > 0 and digit > 0 and special > 0:
+            if upperCase > 0 and lowerCase > 0 and digit > 0 and special > 0:   # all the primary condition satisfya check
                 print("Candidate Password is Strong")
                 break
             else:
-                if lowerCase == 0:
+                if lowerCase == 0:      # lower latter not available in password check
                     print("The alphabet must be between [a-z]")
-                if upperCase == 0:
+                if upperCase == 0:      #  upper latter not available in password check
                     print("At least one alphabet should be of Upper Case [A-Z]")
-                if digit == 0:
+                if digit == 0:          # digit not available in password check
                     print("At least 1 number or digit between [0-9]")
-                if special == 0:
+                if special == 0:        # special charactor available in password check
                     print("At least 1 character from [ _ or @ or $ ]")
 
