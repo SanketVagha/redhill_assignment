242a60c4d55b9a421c39fc04972d9ac1ee9ccc73
SanketVagha <sanketvaghani9@gmail.com>
2024-01-12 16:12:58 +0530

    Completed Thired Question

--- answer3.py
+++ answer3.py
@@ -1,10 +1,11 @@
+
 '''
+
 Question 3. What are the main differences between C and C++?
 
 
 C Language is POP (Program Oriented Language) Oriented Language Means It's Not Refer To The Class and Object.
 
 C++ Language is OOP(Object Oriented Language) Oriented Language Means It's Refer To The Class and Object.
-
 
 '''
