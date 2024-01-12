dadd60ce3846cba203c690d55186ef6c36025748
SanketVagha <sanketvaghani9@gmail.com>
2024-01-12 16:02:27 +0530

    Completed First Question

--- answer1.py
+++ answer1.py
@@ -2,7 +2,6 @@
 Question 1. Write a program to Extract Weather Data from Google in Python. 
 
 '''
-
 import requests
 from bs4 import BeautifulSoup
 
@@ -15,15 +14,15 @@ html = requests.get(url).content
 
 soup = BeautifulSoup(html, 'html.parser')
 
-temp = soup.find('div', attrs={'class': 'BNeawe iBp4i AP7Wnd'}).text
-str = soup.find('div', attrs={'class': 'BNeawe tAd8D AP7Wnd'}).text
+temp = soup.find('div', attrs={'class': 'BNeawe iBp4i AP7Wnd'}).text  # get the temperature
+str = soup.find('div', attrs={'class': 'BNeawe tAd8D AP7Wnd'}).text   #  get the Time, Sky Description
 data = str.split('\n')
 time = data[0]
 sky = data[1]
-listdiv = soup.findAll('div', attrs={'class': 'BNeawe s3v9rd AP7Wnd'})
-strd = listdiv[5].text
-pos = strd.find('Wind')
-other_data = strd[pos:]
+
 print("Temperature is", temp)
 print("Time: ", time)
 print("Sky Description: ", sky)
+
+
+
